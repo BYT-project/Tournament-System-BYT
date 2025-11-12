6acb29e9f007b17d0df18dcb5f4f35e5733e6fc0
WyZzYx <koltok.vitalik@gmail.com>
2025-11-12 17:56:41 +0100

    initial commit: Project creation

---
+++ src/main/java/com/example/tournamentsystembyt/TournamentSystemBytApplication.java
@@ -0,0 +1,13 @@
+package com.example.tournamentsystembyt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TournamentSystemBytApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TournamentSystemBytApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/tournamentsystembyt/TournamentSystemBytApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.tournamentsystembyt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TournamentSystemBytApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
